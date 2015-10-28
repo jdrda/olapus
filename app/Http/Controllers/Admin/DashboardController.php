08b25f35d1385e0244fc16186e96ffc58878b156
Jan Drda <jdrda@outlook.com>
2015-10-28 12:11:09 +0100

    Blank dashboard module

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -19,7 +19,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-       return view('admin/master');
+       return view('admin/modules/dashboard');
     }
 
 }
