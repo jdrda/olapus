8245c8cf15e5037d79925cef891e86628f03b8cb
Jan Drda <jdrda@outlook.com>
2020-04-14 17:52:03 +0200

    Correction of mail from variable

--- config/mail.php
+++ config/mail.php
@@ -83,7 +83,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_EMAIL', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
